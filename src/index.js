c790800e013b6509e76f252ceb4867af28e94375
AhmedSoliman <asol1986ott@gmail.com>
2022-01-11 18:51:19 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
+import './Css/style.css';
+import 'bootstrap';//import 'bootstrap/dist/js/bootstrap.min.js';
+import 'jquery/dist/jquery.min';
+import 'popper.js/dist/popper.min';
